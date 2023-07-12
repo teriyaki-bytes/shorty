959f274193faa834de58fe2635d02fee599bf12e
Tristan Isham <tristan@isham.co>
2023-07-12 00:12:29 -0400

    fixed site URL

--- islands/Shortener.tsx
+++ islands/Shortener.tsx
@@ -29,13 +29,13 @@ export default function Shortener(props?: ShortenerProps) {
                     </button>
                 </form>
             </div>
-            {(props) ? (
+            {(props?.display) ? (
                 <div class="flex items-center justify-center bg-gray-100 p-4" >
                     <div class="w-full max-w-md rounded-md border bg-gray-100 border-gray-400 px-4 py-2 mr-2">
-                        {((SITE_URL === undefined) ? `https://${SITE_URL}` : "http://localhost:8000") + `/${props?.display}`}
+                        {((SITE_URL) ? `https://${SITE_URL}` : "http://localhost:8000") + `/${props?.display}`}
                     </div>
                     <Button
-                        onClick={() => navigator.clipboard.writeText(`https://${SITE_URL || "localhost:8080"}/${props?.display}`)}
+                        onClick={() => navigator.clipboard.writeText(((SITE_URL) ? `https://${SITE_URL}` : "http://localhost:8000") + `/${props?.display}`)}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
                     </Button>
